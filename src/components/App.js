62a0cf05a3621942ebd393989e0e4e90ccb714fd
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-01-26 16:23:28 +0300

    lab done

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,18 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  function handleSubmit() {
+    setPage("List");
+  }
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onFormSubmit={handleSubmit} />
+      ) : (
+        <QuestionList />
+      )}
     </main>
   );
 }
